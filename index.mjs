7d1b564b93415cebfe02e631d7a46e74e48d966a
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-02-28 21:27:50 +0530

    feat: generate metadata containing resolved paths of dependencies for each module

--- index.mjs
+++ index.mjs
@@ -3,6 +3,7 @@ import Resolver from 'jest-resolve';
 import { DependencyResolver } from 'jest-resolve-dependencies';
 import chalk from 'chalk';
 import yargs from 'yargs';
+import fs from 'fs';
 
 import { cpus } from 'os';
 import { dirname, join, resolve } from 'path';
@@ -50,22 +51,48 @@ const dependencyResolver = new DependencyResolver(resolver, hasteFS);
 
 // We use a set to keep a track of all the modules that have been processed
 // This helps us handle circular dependencies by ensuring that we don't process modules more than once
-const processedFiles = new Set();
-const queue = [ entryPoint ];
+const processedModules = new Set();
 
+// we store the code and dependencies of the modules we've processed in this map
+const modulesMetadata = new Map();
+
+const queue = [ entryPoint ];
 while (queue.length) {
     const module = queue.shift();
 
     // If we have already processed this module before
     // we skip processing for this module
-    if (processedFiles.has(module)) {
+    if (processedModules.has(module)) {
         continue;
     }
+    processedModules.add(module);
 
-    processedFiles.add(module);
-    const dependencies = dependencyResolver.resolve(module);
-    queue.push(...dependencies);
+    console.log(chalk.bold(`>>>> Processing ${chalk.green(module)} file`));
+    // Resolve each dependency of the current module and store it in a map
+    // containing its name and actual resolved path of its code
+    const dependencyMap = new Map(
+        hasteFS
+            .getDependencies(module) // get all the dependencies of current module
+            .map((dependencyName) => [  // and for each return its resolved path
+                dependencyName,
+                resolver.resolveModule(module, dependencyName)
+            ]),
+    );
+
+    // read the contents of the current file
+    const contents = fs.readFileSync(module, 'utf-8');
+
+    // and extract the modules defined in this file
+    const moduleBody = contents.match(/module\.exports\s+=\s+(.*?);/)?.[1] || '';
+
+    const metadata = {
+        code: moduleBody || contents,
+        dependencyMap
+    }
+    modulesMetadata.set(module, metadata);
+
+    queue.push( ...dependencyMap.values());
 }
 
-console.log(chalk.bold(`> Found ${chalk.blue(processedFiles.size)} files`));
-console.log(Array.from(processedFiles));
+console.log(chalk.bold(`> Found ${chalk.blue(processedModules.size)} files`));
+console.log(Array.from(modulesMetadata.keys()));
